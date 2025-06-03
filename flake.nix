eeca595baddbd07ee420010d727c47f1c6789d11
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-06-03 13:48:22 -0500

    Trying to debug what is actually in the environment at different stages

--- flake.nix
+++ flake.nix
@@ -8,11 +8,13 @@
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-
-    dioxusWebApp = pkgs.stdenv.mkDerivation {
+    
+    dioxusApp = pkgs.rustPlatform.buildRustPackage {
       pname = "tests-service";
       version = "0.1.0";
       src = ./.;
+
+      cargoLock.lockFile = ./Cargo.lock;
 
       nativeBuildInputs = with pkgs; [
         dioxus-cli
@@ -41,62 +43,79 @@
         wasm-pack          
         lld
       ];
-
-      buildPhase = ''
-        export HOME=$TMPDIR
+      
+      installPhase = ''
+        tree -L 3
         dx bundle --platform web
-        echo "Contents of target:"
-        ls -R target
+        tree -L 3
       '';
-
-    installPhase = ''
-        echo "Attempting to install from:"
-        ls -R target/dx/tests-service/release/web/public || true
-        mkdir -p $out
-        cp -r target/dx/tests-service/release/web/public/* $out/
-      ''; 
     };
 
     nixosModule = { config, lib, pkgs, ... }: {
       options.services.tests-service = {
-        enable = lib.mkEnableOption "Enable Dioxus static web service";
+        enable = lib.mkEnableOption "Status reports for all homelab services";
+
+        port = lib.mkOption {
+          type = lib.types.port;
+          default = 8080;
+          description = "Port to listen on";
+        };
 
         default-nginx = {
-          enable = lib.mkEnableOption "Enable nginx reverse proxy to serve the static site";
+          enable = lib.mkEnableOption "Enable nginx reverse proxy to direct to service";
           hostname = lib.mkOption {
             type = lib.types.str;
             default = "localhost";
-            description = "Hostname to serve the app";
+            description = "Hostname to attach to service";
           };
         };
       };
 
       config = lib.mkIf config.services.tests-service.enable {
-        # Serve the static site via nginx
+        systemd.services.tests-service = {
+          description = "Status page for homelab services";
+          wantedBy = ["multi-user.target"];
+          after = ["network.target"];
+          serviceConfig = {
+            ExecStart = "${dioxusApp}/bin/tests-service}";
+            Restart = "always";
+            Type = "simple";
+            DynamicUser = "yes";
+            WorkingDirectory = "${dioxusApp}";
+          };
+          environment = {
+            PORT = toString config.services.tests-service.port;
+            IP = "0.0.0.0";
+          };
+        };
+
         services.nginx = lib.mkIf config.services.tests-service.default-nginx.enable {
           enable = true;
           virtualHosts."${config.services.tests-service.default-nginx.hostname}" = {
             forceSSL = true;
             enableACME = true;
             acmeRoot = null;
-            root = "${dioxusWebApp}";
             locations."/" = {
-              tryFiles = "$uri $uri/ /index.html";
+              proxyPass = "http://localhost:${toString config.services.tests-service.port}";
             };
           };
         };
 
-        # Open necessary ports
-        networking.firewall.allowedTCPPorts = lib.mkIf config.services.tests-service.default-nginx.enable [
-          80 443
+        networking.firewall.allowedTCPPorts = lib.mkMerge [
+          (lib.mkIf config.services.tests-service.enable [ config.services.tests-service.port ])
+          (lib.mkIf config.services.tests-service.default-nginx.enable [ 80 443 ])
         ];
       };
     };
+    in {
+      packages.${system}.default = dioxusApp;
 
-  in {
-    packages.${system}.default = dioxusWebApp;
-
-    nixosModules.default = nixosModule;
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/tests-service";
+      };
+    }
+    // {
+      nixosModules.default = nixosModule;
   };
 }
-
