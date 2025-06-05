b27e01b0ca48da628c224ff72a4fe5b756a990c2
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-06-05 10:17:49 -0500

    remove config.toml temporarily

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       version = "0.1.0";
       src = ./.;
 
-      cargoLock.lockFile = ./Cargo.lock;
+      cargoVendorDir = ./vendor;
 
       nativeBuildInputs = with pkgs; [
         dioxus-cli
