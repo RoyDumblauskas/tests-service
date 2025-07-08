dfe7c28b0fa40d20465ff03142dc570105101145
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-07-08 11:07:37 -0500

    nav bar good enough

--- src/components/NavBar/NavBar.rs
+++ src/components/NavBar/NavBar.rs
@@ -5,15 +5,21 @@ static CSS: Asset = asset!("/assets/component-css/NavBar.css");
 
 #[component]
 pub fn NavBar() -> Element {
+    let mut pageSelected = use_signal(|| "Home".to_string()); 
+
     rsx! { 
         document::Stylesheet { href: CSS }
         div { id: "navbar",
-            Link {
+            Link { 
+                id: if pageSelected.to_string() == ("Home".to_string()) {"selectedRoute"} else {"unselectedRoute"},
                 to: Route::Home { },
+                onclick: move |_| pageSelected.set("Home".to_string()),
                 "Home"
             }
             Link {
+                id: if pageSelected.to_string() == ("ServiceList".to_string()) {"selectedRoute"} else {"unselectedRoute"},
                 to: Route::ServiceList { },
+                onclick: move |_| pageSelected.set("ServiceList".to_string()),
                 "Service List"
             }
         }
