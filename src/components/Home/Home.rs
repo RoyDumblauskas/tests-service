c52836d6b7929aab10da14ea1a030c8ebee70313
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-07-10 23:39:55 -0500

    revendor for itertools

--- src/components/Home/Home.rs
+++ src/components/Home/Home.rs
@@ -1,8 +1,10 @@
 use dioxus::prelude::*;
+use itertools::Itertools;
 
 static CSS: Asset = asset!("/assets/component-css/Home.css");
+static GH_ICON: Asset = asset!("/assets/github-icon.svg");
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd)]
 struct Project {
    name: String,
    repository: String,
@@ -14,16 +16,22 @@ struct Project {
 pub fn Home() -> Element {
     let projectList: Vec<Project> = vec![ 
     Project {
-        name: "name".to_string(),
+        name: "Laptop Configuration".to_string(),
         repository: "https://github.com/RoyDumblauskas/laptop-config".to_string(),
-        desc: "".to_string(),
+        desc: "Roy's personal laptop configuration with flakes, home-manager, and sops-nix!".to_string(),
+        priority: 2,
+    },
+    Project {
+        name: "Tests Services".to_string(),
+        repository: "https://github.com/RoyDumblauskas/tests-service".to_string(),
+        desc: "Flake that builds and runs a configurable systemd service to monitor the various other services on Roy's homelab. That's this page!".to_string(),
         priority: 0,
     },
     Project {
-        name: "name".to_string(),
-        repository: "repository".to_string(),
+        name: "Homelab Configuration".to_string(),
+        repository: "https://github.com/RoyDumblauskas/homelab-config".to_string(),
         desc: "".to_string(),
-        priority: 0,
+        priority: 1,
     },
     ];
 
