a20560601fc869241746667f32b3a5fab0f765ba
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-07-11 00:13:42 -0500

    ordering of repos

--- src/components/Home/Home.rs
+++ src/components/Home/Home.rs
@@ -18,20 +18,20 @@ pub fn Home() -> Element {
     Project {
         name: "Laptop Configuration".to_string(),
         repository: "https://github.com/RoyDumblauskas/laptop-config".to_string(),
-        desc: "Roy's personal laptop configuration with flakes, home-manager, and sops-nix!".to_string(),
+        desc: "Roy's personal laptop configuration with flakes, home-manager, and sops-nix! Not really a homelab service, but it's pretty cool I think.".to_string(),
         priority: 2,
     },
     Project {
         name: "Tests Services".to_string(),
         repository: "https://github.com/RoyDumblauskas/tests-service".to_string(),
         desc: "Flake that builds and runs a configurable systemd service to monitor the various other services on Roy's homelab. That's this page!".to_string(),
-        priority: 0,
+        priority: 1,
     },
     Project {
         name: "Homelab Configuration".to_string(),
         repository: "https://github.com/RoyDumblauskas/homelab-config".to_string(),
-        desc: "All configuration files for each node in Roy's homelab with flakes, home-manager, and sops-nix. This includes a bootstrap file for the first boot of a machine. The repository also includes the declarations for some homelab services which are not big enough to warrant their own repository (MC & Minio)".to_string(),
-        priority: 1,
+        desc: "All configuration files for each node in Roy's homelab with flakes, home-manager, and sops-nix. This includes a bootstrap file for the first boot of a machine. The repository also includes the declarations for some homelab services which are not big enough to warrant their own repository (MC & Minio).".to_string(),
+        priority: 0,
     },
     ];
 
@@ -40,7 +40,7 @@ pub fn Home() -> Element {
         div { id: "home",
             h2 { "Roy's Homelab Page" }
             div { id: "projectList",
-                {projectList.iter().map(|project| {
+                {projectList.iter().sorted_by(|a, b| Ord::cmp(&a.priority, &b.priority)).map(|project| {
                     rsx! { ProjectElement { project: project.clone() } }
                 })}
             }
