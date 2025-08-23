5e0a90fad1a25ce75d5129024be7f4611cf04589
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-08-23 13:13:32 -0500

    spacing change

--- src/server/GetStatus.rs
+++ src/server/GetStatus.rs
@@ -5,6 +5,7 @@ use std::env;
 pub async fn get_status(url: String) -> Result<String, ServerFnError> {
     // let key = env::var("KEY").unwrap_or("API key does not exist in environment".to_string()); 
     
+
     match env::var("ENV_BUCKET") {
         Ok(val) => Ok(val),
         Err(e) => Ok(e.to_string()),
