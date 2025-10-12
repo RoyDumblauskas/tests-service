62926ee78711ab372e16c854937de38afe8aa742
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-10-12 18:13:33 -0500

    put, list, get, delete methods all interactable

--- src/components/ServiceList/ServiceList.rs
+++ src/components/ServiceList/ServiceList.rs
@@ -1,11 +1,14 @@
 use dioxus::prelude::*;
-use crate::server::{get_status, list_items_in_bucket};
+use crate::server::{get_item_in_bucket, get_status, list_items_in_bucket, put_item_in_bucket, delete_item_in_bucket};
 
 static CSS: Asset = asset!("/assets/component-css/ServiceList.css");
 
 #[component]
 pub fn ServiceList() -> Element {
     let textField = use_signal(|| "".to_string());
+    let fileName = use_signal(|| "".to_string());
+    let itemKey = use_signal(|| "".to_string()); 
+    let deleteItemKey = use_signal(|| "".to_string()); 
     let resp  = use_signal(|| "".to_string()); 
     let itemList: Signal<Vec<String>> = use_signal(|| vec![]); 
 
@@ -13,13 +16,27 @@ pub fn ServiceList() -> Element {
         document::Stylesheet { href: CSS }
         div {
             h2 { "Service List" }
-            ServiceComponent { textField: textField, resp: resp, itemList: itemList}
+            ServiceComponent { 
+                textField: textField, 
+                fileName: fileName, 
+                itemKey: itemKey, 
+                deleteItemKey: deleteItemKey,
+                resp: resp, 
+                itemList: itemList
+            }
         }
     }
 }
 
 #[component]
-fn ServiceComponent(textField: Signal<String>, resp: Signal<String>, itemList: Signal<Vec<String>>) -> Element {
+fn ServiceComponent(
+    textField: Signal<String>, 
+    fileName: Signal<String>, 
+    itemKey: Signal<String>, 
+    deleteItemKey: Signal<String>,
+    resp: Signal<String>, 
+    itemList: Signal<Vec<String>>) 
+    -> Element {
     rsx! {
         div { id: "container",
             h1 { "{resp.clone()}" }
@@ -28,24 +45,79 @@ fn ServiceComponent(textField: Signal<String>, resp: Signal<String>, itemList: S
                     rsx! {h3 { "{item.clone()}" }}
                 })}
             }
-            input {
-                placeholder: "input request url...",
-                oninput: move |event| async move {
-                    let val = event.value();
-                    textField.set(val);
-                }
-            } 
             button {
-                
                 onclick: move |_| async move {
-                    resp.set(get_status(textField.to_string())
-                        .await
-                        .unwrap());
                     itemList.set(list_items_in_bucket()
                         .await
                         .unwrap());
+
+                },
+                "List Items"
+            }
+            div { id: "putObject",
+                input {
+                    placeholder: "Name of file",
+                    value: fileName.clone().to_string(),
+                    oninput: move |event| async move {
+                        fileName.set(event.value());
+                    }
+                }
+                input {
+                    placeholder: "Write string to object",
+                    value: textField.clone().to_string(),
+                    oninput: move |event| async move {
+                        textField.set(event.value());
+                    }
+                } 
+                button {
+                    onclick: move |_| async move {
+                        put_item_in_bucket(fileName.clone().to_string(), textField.clone().to_string())
+                            .await
+                            .unwrap()
+                            .to_string();
+                        fileName.set("".to_string());
+                        textField.set("".to_string());
                     },
-               "Request"
+                    "Put Item"
+                }
+            }
+            div { id: "getItem",
+                input {
+                    placeholder: "Item key",
+                    value: itemKey.clone().to_string(),
+                    oninput: move |event| async move {
+                        itemKey.set(event.value())
+                    }
+                }
+                button {
+                    onclick: move |_| async move {
+                        resp.set(get_item_in_bucket(itemKey.clone().to_string())
+                            .await
+                            .unwrap()
+                            .to_string());
+                        itemKey.set("".to_string());
+                    },
+                    "Get Item"
+                }
+            }
+            div { id: "deleteItem",
+                input {
+                    placeholder: "Item key",
+                    value: deleteItemKey.clone().to_string(),
+                    oninput: move |event| async move {
+                        deleteItemKey.set(event.value())
+                    }
+                }
+                button {
+                    onclick: move |_| async move {
+                        delete_item_in_bucket(deleteItemKey.clone().to_string())
+                            .await
+                            .unwrap()
+                            .to_string();
+                        deleteItemKey.set("".to_string());
+                    },
+                    "Delete Item"
+                }
             }
         }
     }
--- src/server/S3BucketAPIs.rs
+++ src/server/S3BucketAPIs.rs
@@ -4,10 +4,9 @@ use std::env;
 #[server]
 pub async fn list_items_in_bucket() -> Result<Vec<String>, ServerFnError> {
     use crate::server::ServerUtils::server_utils::get_s3_client;
+    let s3 = get_s3_client().await.unwrap();
 
     let env_bucket= env::var("ENV_BUCKET").unwrap_or("Env Var Does not exist".to_string());
-
-    let s3 = get_s3_client().await.unwrap();
 
     // List objects in bucket
     let mut itemsInBucket = s3
@@ -33,7 +32,82 @@ pub async fn list_items_in_bucket() -> Result<Vec<String>, ServerFnError> {
     }
 
     Ok(results)
-
 }
 
+#[server]
+pub async fn put_item_in_bucket(key: String, object: String) -> Result<i32, ServerFnError> {
+    use crate::server::ServerUtils::server_utils::get_s3_client;
+    let s3 = get_s3_client().await.unwrap();
 
+    let env_bucket= env::var("ENV_BUCKET").unwrap_or("Env Var Does not exist".to_string());
+
+    let body = aws_sdk_s3::primitives::ByteStream::from(object.clone().into_bytes());
+
+    let response = s3
+        .put_object()
+        .bucket(env_bucket.to_lowercase().to_owned())
+        .key(key.clone())
+        .body(body)
+        .send()
+        .await;
+
+    match response {
+        Ok(output) => {
+            Ok(0) 
+        }
+        Err(err) => {
+            eprintln!("{err:?}");
+            Ok(-1) 
+        }
+    }
+}
+
+#[server]
+pub async fn get_item_in_bucket(key: String) -> Result<String, ServerFnError> {
+    use crate::server::ServerUtils::server_utils::get_s3_client;
+    let s3 = get_s3_client().await.unwrap();
+
+    let env_bucket= env::var("ENV_BUCKET").unwrap_or("Env Var Does not exist".to_string());
+
+    let item = s3
+        .get_object()
+        .bucket(env_bucket.to_lowercase().to_owned())
+        .key(key.clone())
+        .send()
+        .await;
+
+    match item {
+        Ok(item) => {
+            let bytes = item.body.collect().await?.into_bytes();
+            Ok(String::from_utf8(bytes.to_vec())?)
+        }
+        Err(err) => {
+            Ok(format!("Error: {}", err.to_string()))
+        }
+    }
+}
+
+#[server]
+pub async fn delete_item_in_bucket(key: String) -> Result<i32, ServerFnError> {
+    use crate::server::ServerUtils::server_utils::get_s3_client;
+    let s3 = get_s3_client().await.unwrap();
+
+    let env_bucket= env::var("ENV_BUCKET").unwrap_or("Env Var Does not exist".to_string());
+
+    let resp = s3
+        .delete_object()
+        .bucket(env_bucket.to_lowercase().to_owned())
+        .key(key.clone())
+        .send()
+        .await;
+
+    match resp {
+        Ok(output) => {
+            Ok(0)
+        }
+        Err(err) => {
+            eprintln!("{err:?}");
+            Ok(-1) 
+        }
+    }
+}
